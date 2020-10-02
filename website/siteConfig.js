b1e54600a7389ee5b8f8c04ed8f44bb6a03cf7ce
ai17git <38088737+ai17git@users.noreply.github.com>
2020-10-02 12:40:49 -0700

    Update siteConfig.js

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -15,7 +15,7 @@ const siteConfig = {
     title: 'AI 17', // Title for your website.
     tagline: 'Continuously improving...',
     profilePhoto: 'img/profile.png',
-    url: 'https://anuiyengar.github.io', // Your website URL
+    url: 'https://ai17git.github.io', // Your website URL
     baseUrl: '/tech-portfolio/', // Base URL for your project */
     // For github.io type URLs, you would set the url and baseUrl like:
     //   url: 'https://facebook.github.io',
@@ -23,7 +23,7 @@ const siteConfig = {
 
     // Used for publishing and more
     projectName: 'tech-portfolio',
-    organizationName: 'anuiyengar',
+    organizationName: 'ai17git',
     // For top-level user or org sites, the organization is still the same.
     // e.g., for the https://JoelMarcey.github.io site, it would be set like...
     //   organizationName: 'JoelMarcey'
